6bb3d3b1295688825f57bf679727ed405dad8d23
Michael Burridge <mburridge@zyriab.co.uk>
2020-07-10 12:36:25 +0100

    Initial commit

---
+++ frontity.settings.js
@@ -0,0 +1,63 @@
+const settings = {
+  "name": "vintage-vinyl",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://app-5efddb43c1ac181508283e93.closte.com/wp-json",
+          "postTypes": [
+            {
+              type: "record",
+              endpoint: "record"
+            },
+          ]
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
